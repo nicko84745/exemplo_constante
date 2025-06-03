01671a215652eba349addb9405b8ce6c8a8d4257
NICKOLAS ADRIANO <nickolasadrianodeandradedias@gmail.com>
2025-06-03 15:25:32 -0300

    Add files via upload

---
+++ exemplo_constantes.py
@@ -0,0 +1,6 @@
+# definiçao da constante para a taxa de cambia
+dolar = 5.50
+
+valor_em_dolar = float(input("digite o valor em dolares: "))
+valor_em_dolar = valor_em_dolar * DOLAR 
+print(f"o valor comvertido em reais e: R$ {valor_em_real:.2f}")
